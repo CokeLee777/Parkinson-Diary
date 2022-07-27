aaa75ffd6c1de575273d0cb5374e96a95518a482
CokeLee777 <akak640@kyonggi.ac.kr>
2022-07-27 16:57:49 +0900

    Setting - Add admin web site project file

---
+++ AdminWebSite/src/main/java/healthcare/severance/parkinson/ParkinsonApplication.java
@@ -0,0 +1,13 @@
+package healthcare.severance.parkinson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkinsonApplication.class, args);
+    }
+
+}
---
+++ AdminWebSite/src/test/java/healthcare/severance/parkinson/ParkinsonApplicationTests.java
@@ -0,0 +1,13 @@
+package healthcare.severance.parkinson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
