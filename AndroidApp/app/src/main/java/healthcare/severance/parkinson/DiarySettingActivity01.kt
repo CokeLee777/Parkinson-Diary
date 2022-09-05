59e2320c2fef4a2b5eb67ac653571c54f6f2198d
CokeLee777 <akak640@kyonggi.ac.kr>
2022-09-05 14:52:43 +0900

    feat(diary): Add setting #1,#2 page in android
    다이어리 세팅 1, 2 페이지 이벤트 로직을 구현하였습니다.
    
    #7

---
+++ AndroidApp/app/src/main/java/healthcare/severance/parkinson/DiarySettingActivity01.kt
@@ -0,0 +1,40 @@
+package healthcare.severance.parkinson
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class DiarySettingActivity01 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary_setting01)
+
+        val professors = arrayOf("Dr.KIM", "Dr.LEE", "Dr.PARK")
+        val patient = arrayOf("LEE", "KIM", "PARK")
+
+        initSpinner(findViewById(R.id.sProfessorSpinner), professors)
+        initSpinner(findViewById(R.id.sPatientSpinner), patient)
+    }
+
+    fun initSpinner(spinner: Spinner, spinnerItem: Array<String>){
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerItem)
+        spinner.adapter = adapter
+    }
+
+    fun backToPrevPage(view: View){
+        Log.i(view.resources.getResourceName(view.id),"clicked")
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goToNextPage(view: View){
+        Log.i(view.resources.getResourceName(view.id),"clicked")
+        val intent = Intent(this, DiarySettingActivity02::class.java)
+        startActivity(intent)
+    }
+}
---
+++ AndroidApp/app/src/main/java/healthcare/severance/parkinson/DiarySettingActivity02.kt
@@ -0,0 +1,11 @@
+package healthcare.severance.parkinson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DiarySettingActivity02 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary_setting02)
+    }
+}
