c8902bcbbe5baa25b5127aeb5b92ba2dd84fb719
CokeLee777 <akak640@kyonggi.ac.kr>
2022-11-01 18:56:45 +0900

    feat(Docker): Add Dockerfile and docker-compose file for dev and prod
    개발과 운영 환경을 분리해서 작업하기 위한 도커파일들을 추가하였습니다.

--- ApiServer/parkinson/config/knex.js
+++ ApiServer/parkinson/config/knex.js
@@ -1,13 +1,16 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ 
+    path: path.resolve(__dirname, `../env/${process.env.NODE_ENV}.env`)
+});
 
 const knex = require('knex').knex({
     client: 'mysql2',
     connection: {
-        host: process.env.AWS_RDS_HOST,
-        port: process.env.AWS_RDS_PORT,
-        user: process.env.AWS_RDS_USER,
-        password: process.env.AWS_RDS_PASSWORD,
-        database: process.env.AWS_RDS_DATABASE
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_ROOT_USERNAME,
+        password: process.env.MYSQL_ROOT_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        port: process.env.MYSQL_DATABASE_PORT
     }
 });
 
