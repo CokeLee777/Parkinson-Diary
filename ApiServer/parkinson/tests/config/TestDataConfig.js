0d650576293e379b62b1bd5b3a0af6ba5e9582f7
CokeLee777 <akak640@kyonggi.ac.kr>
2022-11-02 17:37:55 +0900

    Add data config file and ddl file
    데이터 설정정보 파일과 DDL 파일을 추가하였습니다.

--- ApiServer/parkinson/tests/config/TestDataConfig.js
+++ ApiServer/parkinson/tests/config/TestDataConfig.js
@@ -4,7 +4,14 @@ module.exports = {
   testPatientData: {
     patient_num: 121212,
     patient_name: 'admin',
+    sleep_start_time: new Date(`July 1, 1999, 22:00`),
+    sleep_end_time: new Date(`July 1, 1999, 08:00`),
     user_id: 1
   },
+
+  testMedicineData: {
+    patient_num: 121212,
+    take_time: new Date(`July 1, 1999, 12:00`)
+  }
 
 }
