408f1ed4fc56497c3f4eb3f85bfca46ddc7ff680
CokeLee777 <akak640@kyonggi.ac.kr>
2022-11-04 18:23:58 +0900

    Delete trash code

--- AndroidApp/app/src/main/java/healthcare/severance/parkinson/activity/alarm/NotificationProperties.kt
+++
@@ -1,8 +0,0 @@
-package healthcare.severance.parkinson.activity.alarm
-
-object NotificationProperties{
-    const val ALARM_ID = 1
-    const val ALARM_CHANNEL_ID = "alarm_channel_id"
-    const val SURVEY_ID = 2
-    const val SURVEY_CHANNEL_ID = "survey_channel_id"
-}
