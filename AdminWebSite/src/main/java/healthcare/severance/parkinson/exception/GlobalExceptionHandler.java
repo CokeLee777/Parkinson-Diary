148dd6265e3afe86b3a9da58868e756c167ff874
Jeong SeYoung <65009713+JSY8869@users.noreply.github.com>
2023-02-07 18:05:22 +0900

    refac(web) : modify exception handler's method name

--- AdminWebSite/src/main/java/healthcare/severance/parkinson/exception/GlobalExceptionHandler.java
+++ AdminWebSite/src/main/java/healthcare/severance/parkinson/exception/GlobalExceptionHandler.java
@@ -18,20 +18,20 @@ import java.util.Map;
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = { CustomException.class })
-    protected ModelAndView handleCustomException(CustomException e, Model model) {
+    protected ModelAndView customExceptionHandler(CustomException e, Model model) {
         log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
         model.addAttribute("errorCode", e.getErrorCode().getMessage());
         return new ModelAndView("/error/customErrorPage", (Map<String, ?>) model) ;
     }
 
     @ExceptionHandler(value = { ConstraintViolationException.class })
-    protected ModelAndView constraintViolationException(ConstraintViolationException e, Model model) {
+    protected ModelAndView constraintViolationExceptionHandler(ConstraintViolationException e, Model model) {
         log.error("handleCustomException throw CustomException : {}", e.getMessage());
         model.addAttribute("errorCode", e.getMessage());
         return new ModelAndView("/error/customErrorPage", (Map<String, ?>) model) ;
     }
     @ExceptionHandler(value = { AuthenticationException.class })
-    protected ModelAndView AuthenticationExceptionException(AuthenticationException e, Model model) {
+    protected ModelAndView authenticationExceptionHandler(AuthenticationException e, Model model) {
         log.error("handleCustomException throw CustomException : {}", e.getMessage());
         model.addAttribute("errorCode", "권한이 없습니다.");
         return new ModelAndView("/error/customErrorPage", (Map<String, ?>) model) ;
